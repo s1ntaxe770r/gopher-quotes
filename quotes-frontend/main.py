33629e9c20c6bd04184841a38a4bc4144c12c1fc
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-04-28 08:30:05 +0100

    init main route

---
+++ quotes-frontend/main.py
@@ -0,0 +1,22 @@
+from distutils.log import error
+from flask import Flask 
+from os import getenv
+from flask.templating import render_template 
+from typing import List
+import requests
+api_url = getenv("QUOTES_API_URL")
+
+app = Flask(__name__)
+
+
+def GetQuotes(url) -> List[str]:
+    response:List[str] = requests.get(url).json()
+    return response
+
+@app.route("/")
+def index():
+    quotes = GetQuotes(api_url)
+    return render_template("index.html",quotes=quotes)
+
+if __name__=="__main__":
+    app.run(debug=True)
