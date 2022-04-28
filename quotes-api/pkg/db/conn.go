181fb9540b61e0c76f7b9614b665ef43c4cb377c
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-04-28 08:27:26 +0100

    better error handling

--- quotes-api/pkg/db/conn.go
+++ quotes-api/pkg/db/conn.go
@@ -14,28 +14,29 @@ type DB struct {
 
 // NewDB returns a new instance of DB
 func NewDB(dburi string) *DB {
-	conn, err := gorm.Open(postgres.Open(dburi),&gorm.Config{})
+	conn, err := gorm.Open(postgres.Open(dburi), &gorm.Config{})
 	if err != nil {
-		logrus.Panic(logrus.WithFields(logrus.Fields{
-			"msg":"unable to connect to database",
+		logrus.WithFields(logrus.Fields{
+			"msg":    "unable to connect to database",
 			"reason": err.Error(),
-		}))
+		}).Panic("error connecting to database")
 	}
 	err = conn.AutoMigrate(models.Quote{})
 	if err != nil {
-		logrus.Infof("unable to migrate models, reason: %s",err.Error())
+		logrus.Infof("unable to migrate models, reason: %s", err.Error())
 	}
 	return &DB{conn}
 }
+
 // Create inserts a new quote
-func ( db DB) Create(quote models.Quote) error {
-	result := db.conn.Create(&quote) 
+func (db DB) Create(quote models.Quote) error {
+	result := db.conn.Create(&quote)
 	return result.Error
 }
 
 // Get returns all quotes as slice
-func ( db DB ) Get()([]models.Quote,error) {
-     var quotes []models.Quote
-     result := db.conn.Find(&quotes) 
-     return quotes, result.Error
+func (db DB) Get() ([]models.Quote, error) {
+	var quotes []models.Quote
+	result := db.conn.Find(&quotes)
+	return quotes, result.Error
 }
