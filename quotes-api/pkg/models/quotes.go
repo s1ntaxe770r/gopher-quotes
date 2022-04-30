3d6fe9908b2bc0bde7788be3e093c46b4d3c70c6
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-04-30 15:58:04 +0100

    add quote data struct

--- quotes-api/pkg/models/quotes.go
+++ quotes-api/pkg/models/quotes.go
@@ -3,6 +3,10 @@ package models
 // Quote
 type Quote struct {
 	Title string `json:"title"`
-	Body string `json:"body"`
+	Body  string `json:"body"`
 }
 
+type QuoteData struct {
+	Id        int    `json:"-"`
+	LastQuote []byte `json:"last_quote"`
+}
