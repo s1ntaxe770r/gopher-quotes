c32e6c48f53c4c45049f63b73029a459508e69b7
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-05-08 00:31:57 +0100

    change connection mechanim

--- quotes-api/main.go
+++ quotes-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -12,13 +13,17 @@ import (
 )
 
 func main() {
-	uri := os.Getenv("DB_URI")
-	db := db.NewDB(uri)
+	host := os.Getenv("DB_HOST")
+	dbuser := os.Getenv("DB_USER")
+	dbpass := os.Getenv("DB_PASS")
+
+	db_uri := fmt.Sprintf("host=%s user=%s password=%s dbname=quotes port=5432 sslmode=disable", host, dbuser, dbpass)
+	db := db.NewDB(db_uri)
 	h := handlers.NewQoutesHandler(db)
 	r := mux.NewRouter()
 	r.HandleFunc("/create", h.CreateQuote).Methods("POST")
 	r.HandleFunc("/quotes", h.GetQuotes).Methods("GET")
-	r.HandleFunc("/stats",h.GetStats).Methods("GET")
+	r.HandleFunc("/stats", h.GetStats).Methods("GET")
 	r.HandleFunc("/health", handlers.HealthHandler).Methods("GET")
 	log.Println("Starting quotes api on 8080")
 	logrus.Info(http.ListenAndServe(":8080", r))
